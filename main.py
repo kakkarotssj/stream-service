2c6d371de13c8e94d0f40370d333d7f53bd4cd60
kakkarotssj <kanjeckcooldeva.005@gmail.com>
2019-11-20 00:32:04 +0530

    initiated project layout

---
+++ main.py
@@ -0,0 +1,11 @@
+def main():
+    """
+        1). Entry point of this stream-service
+        2). Handle proper start/close of the stream-server
+    """
+    pass
+
+
+if __name__ == '__main__':
+    main()
+
